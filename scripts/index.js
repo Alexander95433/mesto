cc5892cc8d12d08db97368d62bc6a01d2052ac4f
Alexander Komarov <alexkom18@gmail.com>
2022-05-22 16:33:50 +0300

    fix: ревью 2 v 1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,60 +1,42 @@
-//для popup//
-let buttonPopupOn = document.querySelector('.profile__info-button')
-let popup = document.querySelector('.popup')
-let popupClassOn = document.querySelector('.popup_visible')
-let popupClose = document.querySelector('.popup__close')
+//для popup// 
+const buttonPopupOn = document.querySelector('.profile__info-button')
+const popup = document.querySelector('.popup')
+const popupClassOn = document.querySelector('.popup_visible')
+const popupClose = document.querySelector('.popup__close')
 
-//открытие popup//
+//Заполнение popup form// 
+const popupForm = document.querySelector('.popup__form')
+const formName = popupForm.querySelector('.popup__form-name')
+const formDescription = popupForm.querySelector('.popup__form-description')
+const formButton = popupForm.querySelector('.popup__form-button')
+
+//Заполнение профиля// 
+const profileInfoName = document.querySelector('.profile__info-name')
+const profileInfoDescription = document.querySelector('.profile__info-description')
+
+//открытие popup// 
 function openPopup() {
-    popup.classList.toggle('popup_visible')
-}
-buttonPopupOn.addEventListener('click', openPopup)
-
-// закрытие popup  и синхронизировать value с содержанием строк popup//
-function closePopup() {
-
-    popup.classList.remove('popup_visible')
-    //Синхронизирует поля формы и профиля в случае если из popup вышли через popup__close//
+    //Синхронизирует поля формы и профиля в случае если из popup вышли через popup__close// 
     formName.value = profileInfoName.textContent
     formDescription.value = profileInfoDescription.textContent
-
+    popup.classList.add('popup_visible')
 }
-popupClose.addEventListener('click', closePopup)
 
+// закрытие popup  и синхронизировать value с содержанием строк popup// 
+function closePopup() {
+    popup.classList.remove('popup_visible')
+}
 
-//Заполнение popup form//
-let popupForm = document.querySelector('.popup__form')
-let formName = popupForm.querySelector('.popup__form-name')
-let formDescription = popupForm.querySelector('.popup__form-description')
-let formButton = popupForm.querySelector('.popup__form-button')
-
-//Заполнение профиля//
-let profileInfoName = document.querySelector('.profile__info-name')
-let profileInfoDescription = document.querySelector('.profile__info-description')
-
-
-//Не даёт перезаписать value если длинна строки меньше 0//
+//Не даёт перезаписать value если длинна строки меньше 0// 
 function buttonActive() {
-    if (formName.value.length > 0) {
-        if (formDescription.value.length > 0) {
-            profileInfoName.textContent = formName.value
-            profileInfoDescription.textContent = formDescription.value
-
-            closePopup()
-        } else {
-            alert('Ошибка: Заполните все поля ввода')
-        }
-    } else {
-        alert('Ошибка: Заполните все поля ввода')
-    }
+    profileInfoName.textContent = formName.value
+    profileInfoDescription.textContent = formDescription.value
+    closePopup()
 }
+
 formButton.addEventListener('click', buttonActive)
-
-//добавляет plasceholder с подсказкой//
-formName.setAttribute('placeholder', 'Поле не должно быть пустым')
-formDescription.setAttribute('placeholder', 'Поле не должно быть пустым')
-
-
+popupClose.addEventListener('click', closePopup)
+buttonPopupOn.addEventListener('click', openPopup)
 
 
 
