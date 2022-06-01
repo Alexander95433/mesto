61d1e8c025048f0ee51f11441121bce94f441589
Alexander Komarov <alexkom18@gmail.com>
2022-06-01 03:48:47 +0300

    refactor: Добавлен template карточек, значение карточек по умолчанию, активация кнопи=ки like

--- scripts/index.js
+++ scripts/index.js
@@ -11,33 +11,34 @@ const formButton = popupForm.querySelector('.popup__form-button')
 //Заполнение профиля// 
 const profileInfoName = document.querySelector('.profile__info-name')
 const profileInfoDescription = document.querySelector('.profile__info-description')
-//Массив с 6тью исходными карточками//
+//переменные для template//
+const elementHtml = document.querySelector('.element')
 const initialCards = [
     {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
     },
     {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
     },
     {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
     },
     {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
     },
     {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
     },
     {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
     }
-  ]; 
+]
 
 //открытие popup// 
 function openPopup() {
@@ -56,14 +57,36 @@ function formSubmitHandler(evt) {
     profileInfoName.textContent = formName.value
     profileInfoDescription.textContent = formDescription.value
     closePopup()
-
 }
 
+//принимает как аргумент массив карточек по умолчанию  //
+function cards(initialCards) {
+    const elementTemplate = document.querySelector('#element-template').content
+    const elementTemplateClone = elementTemplate.cloneNode(true)
+    const elementPictureClone = elementTemplateClone.querySelector('.element__picture')
+    const elementTitleClone = elementTemplateClone.querySelector('.element__content-title')
+    const likeButtonClone = elementTemplateClone.querySelector('.element__content-button-like')
+    //наполняю template//
+    elementPictureClone.src = initialCards.link
+    elementTitleClone.textContent = initialCards.name
+    //подключаю активацию кнопки лайк//
+    likeButtonClone.addEventListener('click', (event) => {
+        event.target.classList.toggle('element__content-button-like-picture_active')
+    })
+    // возвращаю значение склонированной переменной //
+    return elementTemplateClone
+}
+//расскладываю массив карточек  //
+initialCards.forEach((initialCards) => {
+    elementHtml.append(cards(initialCards))
+})
 
-//начал ратобать с template//
 
 
- 
+//експиримент с темплайт попап//
+const popupClone = document.querySelector('.popup')
+
+
 
 
 
