1eb7bb6232e83c1ea350d088f464e5aaa01b385e
Alexander Komarov <alexkom18@gmail.com>
2022-06-04 18:30:38 +0300

    работа над ревью v1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,25 @@
+
 //для popup-edit-profile// 
 const buttonPopupOn = document.querySelector('.profile__info-button')
-const popup = document.querySelector('.popup-edit-profile')
 const popupClassOn = document.querySelector('.popup_visible')
-const popupClose = document.querySelector('.popup__close-edit-profile')
+const popupCloseEdit = document.querySelector('.popup__close-edit-profile')
+const popupEdit = document.querySelector('.popup-edit-profile')
+
+//popupClose popup
+
 //Заполнение popup form-edit-profile// 
-const popupForm = document.querySelector('.popup__form-edit-profile')
-const formName = popupForm.querySelector('.popup__form-name-edit-profile')
-const formDescription = popupForm.querySelector('.popup__form-description-edit-profile')
-const formButton = popupForm.querySelector('.popup__form-button-edit-profile')
+const popupFormEdit = document.querySelector('.popup__form-edit-profile')
+const formNameEdit = popupFormEdit.querySelector('.popup__form-name-edit-profile')
+const formDescriptionEdit = popupFormEdit.querySelector('.popup__form-description-edit-profile')
+
+//formName formDescription formButton const formButtonEdit = popupFormEdit.querySelector('.popup__form-button-edit-profile')
 
 //Заполнение профиля// 
 const profileInfoName = document.querySelector('.profile__info-name')
 const profileInfoDescription = document.querySelector('.profile__info-description')
 //переменные для template//
-const elementHtml = document.querySelector('.element')
-
+const cardsContainer = document.querySelector('.element')
+//elementHtml
 //Для popup add-a-card// 
 const popupCard = document.querySelector('.popup-add-a-card')
 const buttonCard = document.querySelector('.profile__picture-cross-box')
@@ -25,55 +30,31 @@ const popupFormNameCard = document.querySelector('.popup__form-name-add-a-card')
 const popupFormDescriptionCard = document.querySelector('.popup__form-description-add-a-card')
 
 //popup zoom picture cards
-const elementCard = document.querySelector('.element__card')
 const popupZoomCards = document.querySelector('.popup_zoom-cards')
 const popupZoomCardsPicture = document.querySelector('.popup__picture-zoom-cards ')
 const popupZoomCardsSubtitle = document.querySelector('.popup__subtitle-zoom-cards ')
 const popupCloseZoomCards = document.querySelector('.popup__close_zoom-cards')
 
-const initialCards = [
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
-]
-
+function openPopup (popup) {
+    popup.classList.add('popup_opened');
+ }  
+ 
 //открытие popup// 
 function openPopup() {
     //Синхронизирует поля формы и профиля в случае если из popup вышли через popup__close// 
-    formName.value = profileInfoName.textContent
-    formDescription.value = profileInfoDescription.textContent
-    popup.classList.add('popup_visible')
+    formNameEdit.value = profileInfoName.textContent
+    formDescriptionEdit.value = profileInfoDescription.textContent
+    popupEdit.classList.add('popup_visible')
 }
 // закрывает popup// 
 function closePopup() {
-    popup.classList.remove('popup_visible')
+    popupEdit.classList.remove('popup_visible')
 }
 //Кнопка "сохранить" в popup//
 function formSubmitHandler(evt) {
     evt.preventDefault();
-    profileInfoName.textContent = formName.value
-    profileInfoDescription.textContent = formDescription.value
+    profileInfoName.textContent = formNameEdit.value
+    profileInfoDescription.textContent = formDescriptionEdit.value
     closePopup()
 }
 
@@ -139,7 +120,7 @@ function createCards(Cards) {
 
 //расскладываю массив карточек  //
 initialCards.forEach((Cards) => {
-    elementHtml.append(createCards(Cards))
+    cardsContainer.append(createCards(Cards))
 })
 
 //Добавить новую карточку//
@@ -149,14 +130,14 @@ function formSubmitHandlerCard(evt) {
         name: popupFormNameCard.value,
         link: popupFormDescriptionCard.value
     })
-    elementHtml.prepend(nevCardElement)
+    cardsContainer.prepend(nevCardElement)
     closePopupCard()
 }
 
 //слушатели//
 buttonPopupOn.addEventListener('click', openPopup)
-popupClose.addEventListener('click', closePopup)
-popupForm.addEventListener('submit', formSubmitHandler)
+popupCloseEdit.addEventListener('click', closePopup)
+popupFormEdit.addEventListener('submit', formSubmitHandler)
 buttonCard.addEventListener('click', openPopupCard)
 popupCloseCard.addEventListener('click', closePopupCard)
 popupFormCard.addEventListener('submit', formSubmitHandlerCard)
