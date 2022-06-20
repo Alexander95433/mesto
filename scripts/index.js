5937d39e2031c7fb4fe9353a0bfa201bf1b69325
Alexander Komarov <alexkom18@gmail.com>
2022-06-21 02:26:15 +0300

    refactor: исправлеы механизмы открытия и закрытия popup

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,6 @@
 //для popup-edit-profile// 
 const buttonPopupOn = document.querySelector('.profile__info-button');
 const popupClassOn = document.querySelector('.popup_visible');
-const popupCloseEdit = document.querySelector('.popup__close-edit-profile');
 const popupEdit = document.querySelector('.popup-edit-profile');
 const popupFormEdit = document.querySelector('.popup__form-edit-profile');
 const formNameEdit = popupFormEdit.querySelector('.popup__form-input_name-edit-profile');
@@ -16,7 +15,6 @@ const cardsContainer = document.querySelector('.element');
 //Для popup add-a-card// 
 const popupCard = document.querySelector('.popup-add-a-card');
 const buttonCard = document.querySelector('.profile__picture-cross-box');
-const popupCloseCard = document.querySelector('.popup__close-add-a-card');
 const buttonCreatePopupCard = document.querySelector('.popup__form-button');
 const popupFormCard = document.querySelector('.popup__form-add-a-card');
 const popupFormNameCard = document.querySelector('.popup__form-input_name-add-a-card');
@@ -28,16 +26,18 @@ const popupZoomCardsSubtitle = document.querySelector('.popup__subtitle-zoom-car
 const popupCloseZoomCards = document.querySelector('.popup__close_zoom-cards');
 //popup для закрытия по click на overlay
 const popups = document.querySelectorAll('.popup');
-const popupGlobal = document.querySelector('.popup');
+const closePopupGlobal = document.querySelector('.popup__close')
 
 //универсальная функция для открытия popup//
 function openPopup(popup) {
     popup.classList.add('popup_visible');
+    document.addEventListener('keydown', closeByEscape)
 };
 
 //универсальная функция для закрытия popup//
 function closePopup(popup) {
     popup.classList.remove('popup_visible');
+    document.removeEventListener('keydown', closeByEscape)
 };
 
 //открытие popup// 
@@ -49,17 +49,12 @@ function openPopupEdit() {
     openPopup(popupEdit);
 };
 
-// закрывает popup// 
-function closePopupEdit() {
-    closePopup(popupEdit);
-};
-
 //Кнопка "сохранить" в popup//
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
     profileInfoName.textContent = formNameEdit.value;
     profileInfoDescription.textContent = formDescriptionEdit.value;
-    closePopupEdit();
+    closePopup(popupEdit);
 };
 
 //открыть popup add-a-card//
@@ -69,10 +64,16 @@ function openPopupCard() {
     openPopup(popupCard);
 };
 
-//закрыть popup add-a-card//
-function closePopupCard() {
-    closePopup(popupCard);
-};
+//Закрывает все popup по клику на кнопку закрытия и на overlay
+popups.forEach((popupElement) => {
+    popupElement.addEventListener('mousedown', (evt) => {
+        if (evt.target.classList.contains('popup_visible')) {
+            closePopup(popupElement);
+        };
+        if (evt.target.classList.contains('popup__close'))
+            closePopup(popupElement);
+    });
+});
 
 //Закрываю popup по click на overlay
 popups.forEach((popupElement) => {
@@ -84,13 +85,12 @@ popups.forEach((popupElement) => {
 });
 
 //Закрываю popup кнопкой Escape
-popups.forEach((popupElement) => {
-    document.addEventListener('keydown', (evt) => {
-        if (evt.key === 'Escape') {
-            closePopup(popupElement);
-        };
-    });
-});
+function closeByEscape(evt) {
+    if (evt.key === 'Escape') {
+        const openPopup = document.querySelector('.popup_visible');
+        closePopup(openPopup);
+    };
+};
 
 //подключаю активацию кнопки лайк//
 function handleLikeCard(event) {
@@ -102,23 +102,13 @@ function deleteCard(event) {
     event.target.closest('.element__card').remove();
 };
 
-//Открываю popup zoom //
-function zoomPictureCardsOn() {
-    openPopup(popupZoomCards);
-};
-
-//закрываю popup с zoom//
-function zoomPictureCardsOff() {
-    closePopup(popupZoomCards);
-};
-
 //Привязываю карточки к popup zoom
-function popupZoom(card) {
+function openPopupZoom(card) {
     popupZoomCardsPicture.src = card.link;
     popupZoomCardsPicture.alt = card.name;
     popupZoomCardsSubtitle.textContent = card.name;
     //Открываю popup zoom //
-    zoomPictureCardsOn();
+    openPopup(popupZoomCards);
 };
 
 //создаю карточки  //
@@ -133,13 +123,12 @@ function createCard(card) {
     elementPictureClone.src = card.link;
     elementPictureClone.alt = card.name;
     elementTitleClone.textContent = card.name;
-
     //подключаю активацию кнопки лайк//
     likeButtonClone.addEventListener('click', handleLikeCard);
     //удаляю карточку//
     buttonTrashElement.addEventListener('click', deleteCard);
     //открываю popup увеличение изображения карточки//
-    elementPictureClone.addEventListener('click', function () { popupZoom(card) });
+    elementPictureClone.addEventListener('click', function () { openPopupZoom(card) });
     // возвращаю значение склонированной переменной //
     return elementTemplateClone;
 };
@@ -159,17 +148,12 @@ function handleCardFormSubmit(evt) {
     });
     cardsContainer.prepend(nevCardElement);
     popupFormCard.reset();
-    closePopupCard();
+    closePopup(popupCard);
 };
-
-
 
 //слушатели//
 buttonPopupOn.addEventListener('click', openPopupEdit);
-popupCloseEdit.addEventListener('click', closePopupEdit);
 popupFormEdit.addEventListener('submit', handleProfileFormSubmit);
 buttonCard.addEventListener('click', openPopupCard);
-popupCloseCard.addEventListener('click', closePopupCard);
 popupFormCard.addEventListener('submit', handleCardFormSubmit);
-popupCloseZoomCards.addEventListener('click', zoomPictureCardsOff);
 
